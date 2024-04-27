fe422dfbdaa8d0de23c556eac074d6c90c1e6faf
fili5rovic <fili5rovic@gmail.com>
2024-04-27 20:17:10 +0200

    Added regex for where

--- Select.cpp
+++ Select.cpp
@@ -1,3 +1,4 @@
 #include "Select.h"
 
-const regex Select::regexPattern = regex(R"(^\s*select\s+((?:\w+|\*)(\s*,\s*(?:\w+|\*))*)\s+from\s+(\w+)+)", regex_constants::icase);
+//const regex Select::regexPattern = regex(R"(^\s*select\s+((?:\w+|\*)(\s*,\s*(?:\w+|\*))*)\s+from\s+(\w+)+)", regex_constants::icase);
+const regex Select::regexPattern = regex(R"(^\s*select\s+((?:\w+|\*)(?:\s*,\s*(?:\w+|\*))*)\s+from\s+(\w+)+(\s+where\s+(\w+)\s*(\=|\<\>|\!\=)\s*(\w+)\s+(and\s*(\w+)\s*(\=|\<\>|\!\=)\s*(\w+)\s*)*)?)", regex_constants::icase);
